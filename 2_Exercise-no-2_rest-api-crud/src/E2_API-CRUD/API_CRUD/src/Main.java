9fa08047f942144ce476fbe146e48f486971005e
luqasz-tworzydlo <72106587+luqasz-tworzydlo@users.noreply.github.com>
2023-12-09 13:07:58 +0100

    Add files via upload

--- 2_Exercise-no-2_rest-api-crud/src/E2_API-CRUD/API_CRUD/src/Main.java
+++ 2_Exercise-no-2_rest-api-crud/src/E2_API-CRUD/API_CRUD/src/Main.java
@@ -5,46 +5,53 @@ import java.net.http.HttpResponse;
 import java.net.http.HttpRequest.BodyPublishers;
 import java.net.http.HttpResponse.BodyHandlers;
 
-class JsonPlaceholderExample {
+class JSON_Placeholder_FakeAPI {
     private static final String BASE_URL = "https://jsonplaceholder.typicode.com";
+    // {JSON} Placeholder Free fake API for testing and prototyping.
+    // {JSON} Placeholder to strona z darmowym fałszywym API.
+    // Link do strony to: https://jsonplaceholder.typicode.com/
 
     public static void main(String[] args) throws Exception {
         HttpClient client = HttpClient.newHttpClient();
 
-        // GET request to retrieve posts
+        // Metoda GET ( zapytanie o pobranie wpisu )
         HttpRequest getRequest = HttpRequest.newBuilder()
                 .uri(URI.create(BASE_URL + "/posts"))
                 .GET()
                 .build();
         HttpResponse<String> getResponse = client.send(getRequest, BodyHandlers.ofString());
-        System.out.println("GET Response: " + getResponse.body());
+        System.out.println("\n=> Odpowiedź dla GET: " + getResponse.body());
+        System.out.println("\n/// /// /// /// /// /// /// /// ///");
 
-        // POST request to create a new post
-        String newPostJson = "{\"title\": \"foo\", \"body\": \"bar\", \"userId\": 1}";
+        // Metoda POST ( zapytanie o stworzenie nowego wpisu )
+        String NowyWpisJSON = "{\"title\": \"foo\", \"body\": \"bar\", \"userId\": 1}";
         HttpRequest postRequest = HttpRequest.newBuilder()
                 .uri(URI.create(BASE_URL + "/posts"))
                 .header("Content-Type", "application/json")
-                .POST(BodyPublishers.ofString(newPostJson))
+                .POST(BodyPublishers.ofString(NowyWpisJSON))
                 .build();
         HttpResponse<String> postResponse = client.send(postRequest, BodyHandlers.ofString());
-        System.out.println("POST Response: " + postResponse.body());
+        System.out.println("\n=> Odpowiedź dla POST: " + postResponse.body());
+        System.out.println("\n/// /// /// /// /// /// /// /// ///");
 
-        // PUT request to update an existing post (id 1 for example)
-        String updatedPostJson = "{\"id\": 1, \"title\": \"foo updated\", \"body\": \"bar updated\", \"userId\": 1}";
+        // Metoda PUT ( zapytanie o aktualizację istniejącego, wybranego wpisu )
+        String ZaktualizowanyWpisJSON = "{\"id\": 1, \"title\": \"foo updated\", \"body\": \"bar updated\", \"userId\": 1}";
         HttpRequest putRequest = HttpRequest.newBuilder()
                 .uri(URI.create(BASE_URL + "/posts/1"))
                 .header("Content-Type", "application/json")
-                .PUT(BodyPublishers.ofString(updatedPostJson))
+                .PUT(BodyPublishers.ofString(ZaktualizowanyWpisJSON))
                 .build();
         HttpResponse<String> putResponse = client.send(putRequest, BodyHandlers.ofString());
-        System.out.println("PUT Response: " + putResponse.body());
+        System.out.println("\n=> Odpowiedź dla PUT: " + putResponse.body());
+        System.out.println("\n/// /// /// /// /// /// /// /// ///");
 
-        // DELETE request to delete a post (id 1 for example)
+        // Metoda DELETE ( zapytanie o usunięcie wybranego, konkretnego wpisu )
         HttpRequest deleteRequest = HttpRequest.newBuilder()
                 .uri(URI.create(BASE_URL + "/posts/1"))
                 .DELETE()
                 .build();
         HttpResponse<String> deleteResponse = client.send(deleteRequest, BodyHandlers.ofString());
-        System.out.println("DELETE Response: " + deleteResponse.body());
+        System.out.println("\n=> Odpowiedź dla DELETE: " + deleteResponse.body());
+        System.out.println("\n/// /// /// /// /// /// /// /// ///");
     }
 }
